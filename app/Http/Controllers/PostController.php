de6d2a41f1e213bfdff3943566a7224b8d619bec
YounessXD <yns.barka@gmail.com>
2023-03-07 16:06:13 +0000

    api connected to the tieweb website and create and display new posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Post::get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->post = $request->input('post');
+        $post->save();
+
+        return response()->json([
+            'message' => 'Post created successfully',
+            'post' => $post
+          ]);
+        // Post::create([
+        //     'post' => $request->post,
+        // ]);
+        // return responce()->json('good');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
